b63356a033a9097680c0f43d363103f3fabd88a0
Robb Gatica <info@robbgatica.com>
2022-06-26 22:16:04 -0500

    added success msg to connectivity test

--- ansible_setup/host_setup.sh
+++ ansible_setup/host_setup.sh
@@ -2,10 +2,10 @@
 
 ssh-keygen -q -t rsa -f /home/ansible/.ssh/id_rsa -N ''
 
-ansible all -m user -a "name=norman state=present password={{ 'password' | password_hash('sha512', 'mysecretsalt') }}" --extra-vars "ansible_user=root ansible_password=password"
+ansible all -m user -a "name=ansible state=present password={{ 'password' | password_hash('sha512', 'mysecretsalt') }}" --extra-vars "ansible_user=root ansible_password=password"
 
-ansible all -m shell -a "echo 'norman ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/norman" --extra-vars "ansible_user=root ansible_password=password"
+ansible all -m shell -a "echo 'ansible ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/ansible" --extra-vars "ansible_user=root ansible_password=password"
 
-ansible all -m authorized_key -a "user=norman state=present key={{ lookup('file', '/home/ansible/.ssh/id_rsa.pub') }}" --extra-vars "ansible_user=root ansible_password=password"
+ansible all -m authorized_key -a "user=ansible state=present key={{ lookup('file', '/home/ansible/.ssh/id_rsa.pub') }}" --extra-vars "ansible_user=root ansible_password=password"
 
 ansible all -m ping
